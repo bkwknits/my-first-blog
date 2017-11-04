488a5ee1aa8a7ca134512d0e2fb00985bd8ec474
Bkwknits <bkwknits@gmail.com>
2017-11-04 15:36:00 -0700

    My Django Girls app, first commit

---
+++ Python_intro.py
@@ -0,0 +1,56 @@
+print('Hello, Django Girls')
+if 3>2:
+	print('It works!')
+if 5>2:
+	print('5 is indeed greater than 2')
+else:
+	print('5 is not greater than 2')
+name = 'Brenda'
+if name == 'Ola':
+	print('Hey Ola!')
+elif name == 'Brenda':
+	print ('Hey Brenda!')
+else:
+	print('Hey anonymous!')
+volume=57
+if volume < 20:
+	print("It's kinda quiet.")
+elif 20<= volume < 40:
+	print("It's nice for background music")
+elif 40 <= volume <60:
+	print("Perfect, I can hear all the details")
+elif 60<= volume < 80:
+	print ("nice for parties")
+elif 80<= volume <100:
+	print("A bit loud!")
+else:
+	print ("My ears are hurting! :(")
+# Change the volume if it's too loud or too quiet
+if volume <20 or volume > 80:
+	volume = 50
+	print("That's better!")
+def hi():
+	print("Hi there!")
+	print("How are you?")
+
+hi()
+
+def hi(name):
+	if name == 'Ola':
+		print('Hi Ola!')
+	elif name == 'Brenda':
+		print('Hi Brenda!')
+	else:
+		print('Hi anonymous!')
+
+hi("Brenda")
+
+def hi(name):
+	print('Hi ' + name+'!')
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+	hi(name)
+	print('Next girl')
+
+for i in range(1,6):
+	print(i)
